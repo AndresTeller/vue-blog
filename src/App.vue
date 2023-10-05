9a2bfcce061a6abfd7da9a711bcb6b2cc5f15ce9
Andres Teller <andres.teller2@gmail.com>
2023-10-05 12:10:06 -0500

    feat: add navbar

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import NavbarMobile from "@/components/NavbarMobile.vue";
 import RecentBlogPosts from "./components/RecentBlogPosts.vue";
 import AllBlogPosts from "./components/AllBlogPosts.vue";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
+  <Navbar />
   <NavbarMobile />
   <RecentBlogPosts />
   <AllBlogPosts />
---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { navLinks } from "@/constants";
+</script>
+
+<template>
+  <nav
+    class="hidden md:flex justify-between items-center container mx-auto pt-5"
+  >
+    <a href="/">
+      <span class="text-2xl font-bold">AndrésTeller</span>
+    </a>
+    <div class="flex gap-x-10">
+      <a
+        class="text-slate-900 hover:text-slate-900/75"
+        v-for="navlink in navLinks"
+        :href="navlink.href"
+        :key="navlink.id"
+      >
+        {{ navlink.label }}
+      </a>
+    </div>
+  </nav>
+</template>
