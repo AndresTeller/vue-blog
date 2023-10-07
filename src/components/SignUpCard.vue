fdf8d91abc74a1af91c3c48a7c8ee2d6b3898a06
Andres Teller <andres.teller2@gmail.com>
2023-10-07 01:52:17 -0500

    refactor: sign up form

--- src/components/SignUpCard.vue
+++ src/components/SignUpCard.vue
@@ -1,97 +1,162 @@
 <script setup lang="ts">
-import { Card } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { useForm } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/zod";
+import * as z from "zod";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Card } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { ref } from "vue";
 
-const user = ref({
-  name: "",
-  lastname: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
-  isConfirmedTermsAndConditions: false,
-})
+const isOpen = ref(false);
 
+const formSchema = toTypedSchema(
+  z.object({
+    name: z.string().min(3).max(20),
+    lastname: z.string().min(3).max(20),
+    email: z.string().email(),
+    password: z.string().min(5),
+    confirmPassword: z.string().min(5),
+    isConfirmedTermsAndConditions: z.boolean(),
+  })
+);
 
+const form = useForm({
+  validationSchema: formSchema,
+});
+
+const onOpenChange = (value: boolean) => {
+  isOpen.value = value;
+}
+
+const onSubmit = form.handleSubmit((values) => {
+  const isPasswordWrong = values.confirmPassword !== values.password;
+  onOpenChange(isPasswordWrong);
+});
 </script>
 
 <template>
   <Card class="flex flex-col w-full gap-y-5 max-w-md px-10 py-5">
     <span class="text-3xl font-bold">Create an account</span>
+    <form @submit="onSubmit" class="flex flex-col gap-y-5">
+      <FormField v-slot="{ componentField }" name="name">
+        <FormItem>
+          <FormLabel>Name</FormLabel>
+          <FormControl>
+            <Input
+              class="focus-visible:ring-transparent"
+              type="text"
+              placeholder="John"
+              v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
 
-    <div>
-      <Label for="name">Name</Label>
-      <Input
-        type="text"
-        placeholder="John"
-        id="name"
-        class="p-6 focus-visible:ring-transparent"
-        v-model="user.name"
-      />
-    </div>
-    <div>
-      <Label for="lastname">Lastname</Label>
-      <Input
-        type="text"
-        placeholder="Doe"
-        id="lastname"
-        class="p-6 focus-visible:ring-transparent"
-        v-model="user.lastname"
-      />
-    </div>
-    <div>
-      <Label for="email">Your email</Label>
-      <Input
-        type="email"
-        placeholder="Email"
-        id="email"
-        class="p-6 focus-visible:ring-transparent"
-        v-model="user.email"
-      />
-    </div>
-    <div>
-      <Label for="password">Password</Label>
-      <Input
-        type="password"
-        placeholder="password"
-        id="password"
-        class="p-6 focus-visible:ring-transparent"
-        v-model="user.password"
-      />
-    </div>
-    <div>
-      <Label for="password">Confirm password</Label>
-      <Input
-        type="password"
-        placeholder="password"
-        id="confirmPassword"
-        class="p-6 focus-visible:ring-transparent"
-        v-model="user.confirmPassword"
-      />
-    </div>
+      <FormField v-slot="{ componentField }" name="lastname">
+        <FormItem>
+          <FormLabel>Lastname</FormLabel>
+          <FormControl>
+            <Input
+              class="focus-visible:ring-transparent"
+              type="text"
+              placeholder="Doe"
+              v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
 
-    <div class="flex justify-between">
-      <div class="flex items-center space-x-2">
-        <Checkbox id="terms" v-model="user.isConfirmedTermsAndConditions"/>
-        <label
-          htmlFor="terms"
-          class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-        >
-          I accept the Terms and Conditions
-        </label>
-      </div>
-    </div>
-    <Button>Sign in</Button>
-    <p>
-      Already have an account?
-      <router-link
-        :to="{ name: 'SignUp' }"
-        class="text-blue-800 hover:text-blue-800/75"
-        >Login here</router-link
+      <FormField v-slot="{ componentField }" name="email">
+        <FormItem>
+          <FormLabel>Email</FormLabel>
+          <FormControl>
+            <Input
+              class="focus-visible:ring-transparent"
+              type="text"
+              placeholder="johndoe@email.com"
+              v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <FormField v-slot="{ componentField }" name="password">
+        <FormItem>
+          <FormLabel>Password</FormLabel>
+          <FormControl>
+            <Input
+              class="focus-visible:ring-transparent"
+              type="password"
+              placeholder="password"
+              v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <FormField v-slot="{ componentField }" name="confirmPassword">
+        <FormItem>
+          <FormLabel>Confirm Password</FormLabel>
+          <FormControl>
+            <Input
+              class="focus-visible:ring-transparent"
+              type="password"
+              placeholder="password"
+              v-bind="componentField"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      </FormField>
+
+      <FormField
+        v-slot="{ value, handleChange }"
+        name="isConfirmedTermsAndConditions"
       >
-    </p>
+        <FormItem
+          class="flex flex-row items-start gap-x-3 space-y-0 rounded-md p-4"
+        >
+          <FormControl>
+            <Checkbox :checked="value" @update:checked="handleChange" />
+          </FormControl>
+          <div class="space-y-1 leading-none">
+            <FormLabel>Accept Terms and Conditions</FormLabel>
+            <FormMessage />
+          </div>
+        </FormItem>
+      </FormField>
+
+      <Button type="submit" class="w-full">
+        Create an account
+      </Button>
+    </form>
   </Card>
+
+  <Dialog :open="isOpen" @update:open="onOpenChange">
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Check your password</DialogTitle>
+        <DialogDescription> The passwords did not match </DialogDescription>
+      </DialogHeader>
+    </DialogContent>
+  </Dialog>
 </template>
