4e7bc731e88b6a7300fb29b976ea07c249daf9ce
Andres Teller <andres.teller2@gmail.com>
2023-10-08 23:41:12 -0500

    feat: add sign out laptop and desktop functionality

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import { navLinks } from "@/constants";
+import SignOut from "@/components/SignOut.vue";
+import { useUserStore } from "@/store/useUserStore";
+
+const userStore = useUserStore();
 </script>
 
 <template>
@@ -10,14 +14,16 @@ import { navLinks } from "@/constants";
       <span class="text-2xl font-bold">AndrésTeller</span>
     </router-link>
     <div class="flex gap-x-10">
-      <router-link
-        class="text-slate-900 hover:text-slate-900/75"
-        v-for="navlink in navLinks"
-        :to="{ name: navlink.name }"
-        :key="navlink.id"
-      >
-        {{ navlink.label }}
-      </router-link>
+      <div v-for="navlink in navLinks">
+        <SignOut v-if="userStore.isAuthenticated && navlink.name === 'Login'" />
+        <router-link
+          class="text-slate-900 hover:text-slate-900/75"
+          :to="{ name: navlink.name }"
+          v-else="userStore.isAuthenticated && navlink.name === 'Login'"
+        >
+          {{ navlink.label }}
+        </router-link>
+      </div>
     </div>
   </nav>
 </template>
---
+++ src/components/SignOut.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { useUserStore } from "@/store/useUserStore";
+import { LogOut } from "lucide-vue-next";
+import { useRouter } from "vue-router";
+
+const userStore = useUserStore();
+const router = useRouter();
+
+const handleClick = () => {
+  userStore.logOut();
+  router.push({name: "Login"});
+};
+
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger class="text-slate-900 hover:text-slate-900/75"
+      >Sign out</DropdownMenuTrigger
+    >
+    <DropdownMenuContent>
+      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem class="cursor-pointer" @click="handleClick">
+        <LogOut class="mr-2 h-4 w-4" />
+        <span>Log out</span>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
